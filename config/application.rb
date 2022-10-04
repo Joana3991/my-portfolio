62584692acfe2507a27ac5b7ce82752b296e8266
Joana Areas <jo_areas@hotmail.com>
2022-10-04 00:24:54 -0300

    background black

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module MyPortfolio
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
   end
 end
